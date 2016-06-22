7ad560a4d6eac9352914b4f684eae13b9bc83f70
liuk <liukan@126.com>
2016-06-22 19:00:51 +0800

    ui

--- src/org/liukan/xplorSUI/crossLinkingGen.java
+++ src/org/liukan/xplorSUI/crossLinkingGen.java
@@ -1,7 +1,135 @@
 package org.liukan.xplorSUI;
 
+import org.liukan.mgraph.graphStru;
+import org.liukan.mgraph.medge;
+import org.liukan.mgraph.mnode;
+
+import java.util.*;
+
 /**
  * Created by liuk on 2016/6/14.
  */
 public class crossLinkingGen {
+    private final graphStru gs;
+    private  String segidPrefix;
+
+
+    public static <T, E> Set<T> getKeysByValue(Map<T, E> map, E value) {
+        Set<T> keys = new HashSet<T>();
+        for (Map.Entry<T, E> entry : map.entrySet()) {
+            if (Objects.equals(value, entry.getValue())) {
+                keys.add(entry.getKey());
+            }
+        }
+        return keys;
+    }
+    public crossLinkingGen(graphStru gs) {
+        segidPrefix="XL";
+        procedNodes=new HashMap<mnode,String>();
+        this.gs=gs;
+        segID=new TreeMap<String,String>();
+        segID2=new TreeMap<String,String>();
+    }
+    public void genSegid(){
+        int size = gs.nodes.size();
+        for (int j = 0; j < size; j++) {
+            mnode n = gs.nodes.get(j);
+            if (n.id.length() < 1)
+                continue;
+            String sid=assignSegid(n);
+            System.out.println(sid+","+n.label);
+        }
+        System.out.println("===###="+segID);
+
+        size = gs.edges.size();
+        for (int j = 0; j < size; j++) {
+            medge e = gs.edges.get(j);
+            if (e.id.length() < 1)
+                continue;
+            System.out.println(dupSegid(gs.getNode(e.source),gs.getNode(e.target))+","+gs.getNode(e.target).label);
+        }
+
+    }
+
+    private String dupSegid(mnode source, mnode target) {
+        String ssid=procedNodes.get(source);
+        String tsid=procedNodes.get(target);
+        if(ssid==null||tsid==null)
+            return null;
+        if(ssid.equals(tsid))
+            return null;
+        Set<String> keys=getKeysByValue(segID2,ssid);
+        if(keys.size()>1) {
+            for (String nodeid : keys
+                    ) {
+                System.out.println("=2=="+nodeid.equals(source.id)+source.label);
+            }
+        }else
+            System.out.println("=1=="+keys.toArray()[0]);
+        return ssid;
+    }
+
+    /**
+     * (n,segid)
+     */
+    private Map<mnode,String> procedNodes;
+    /**
+     * Map<String,String> segID<n.id,segid>
+     */
+    private Map<String,String> segID;
+    private Map<String,String> segID2;
+
+    private String assignSegid(mnode n) {
+        LinkedList<mnode> cn=getConnectedNodes(n);//获取相连接的点
+        String segid=null;
+        if(cn.size()>1) {
+            segid = findSegid(cn);//已处理点中 是否有点连接点 如果有返回相连接点sid
+
+        }
+        if(segid==null)
+            segid=genNewSegid(n);
+        procedNodes.put(n,segid);
+        segID2.put(n.id,segid);
+        return segid;
+    }
+
+    private LinkedList<mnode> getConnectedNodes(mnode n) {
+        LinkedList<mnode> rv=new LinkedList<mnode>();
+        int size = gs.edges.size();
+        for (int j = 0; j < size; j++) {
+            medge e=gs.edges.get(j);
+            if(e.source.length()<1||e.target.length()<1)
+                continue;
+            if(n.id.equals(e.source))
+                rv.add(gs.getNode(e.target));
+            else if(n.id.equals(e.target))
+                rv.add(gs.getNode(e.source));
+        }
+        return rv;
+    }
+
+    private String findSegid(LinkedList<mnode> cn) {
+        int size = cn.size();
+        String segid=null;
+        for (int j = 0; j < size; j++) {
+            mnode n = cn.get(j);
+            segid = segID.get(n.id);
+            if (segid != null)
+                break;
+        }
+        return segid;
+    }
+    private String genNewDupid(String s,String t) {
+        String segid;
+        segid=s+segidPrefix+t;
+        return segid;
+    }
+    private String genNewSegid(mnode n) {
+        String segid;
+        segid=segidPrefix+n.id;
+        segID.put(n.id,segid);
+        return segid;
+    }
+
+
 }
--- src/org/liukan/xplorSUI/crossLinkingUI.java
+++ src/org/liukan/xplorSUI/crossLinkingUI.java
@@ -3,70 +3,58 @@ package org.liukan.xplorSUI;
 import org.liukan.mgraph.mgraphxEx;
 import org.liukan.mgraph.util.dbIO;
 import org.liukan.xplorSUI.db.thePath;
+import org.liukan.xplorSUI.ui.CtrlInputPanel;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
 import java.util.Locale;
+
+import static com.sun.java.accessibility.util.AWTEventMonitor.addWindowListener;
 
 /**
  * Created by liuk on 2016/6/14.
  */
 public class crossLinkingUI {
-    static String dbUrl = thePath.getPath()+ File.separator+"db"+File.separator+"db.sqlite";
-    public static void main(String[] args)
-    {
+    String dbUrl;
+    dbIO dbio;
+
+    public crossLinkingUI() {
+        dbUrl = thePath.getPath()+ File.separator+"db"+File.separator+"db.sqlite";
+        dbio=new dbIO("org.sqlite.JDBC","jdbc:sqlite:"+dbUrl,null,null);
         JFrame  frame = new JFrame("DynaXL");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setLayout(new BorderLayout());
-        Locale cl=new Locale("en", "US");
+        Locale cl=new Locale("en", "US","BIO");
         mgraphxEx c=new mgraphxEx(cl);
-        c.gpanel.setupGraphStyle(20,22,2,false);
-        //c.setSize(880, 500);
-        JPanel panel_button = new JPanel();
-        JButton btnNewNodeButton = new JButton("ReadCross-linksMode");
-        btnNewNodeButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                try {
-                    dbIO dbio=new dbIO("org.sqlite.JDBC","jdbc:sqlite:"+dbUrl,null,null);
-                    c.gpanel.readGfromDB(dbio,2);
-                    dbio.close();
-                } catch ( Exception e ) {
-                    System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-                    //System.exit(0);
-                }
-            }
-        });
-        panel_button.add(btnNewNodeButton);
-
-        JButton btnSaveButton = new JButton("SaveCross-linksMode");
-        btnSaveButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                try {
-                    dbIO dbio=new dbIO("org.sqlite.JDBC","jdbc:sqlite:"+dbUrl,null,null);
-                    c.gpanel.saveG2DB("hoho",2,dbio);
-                    dbio.close();
-                } catch ( Exception e ) {
-                    System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-                    //System.exit(0);
-                }
-            }
-        });
-        panel_button.add(btnSaveButton);
-        JButton GenButton = new JButton("GenCross-linksConfigure");
-        panel_button.add(GenButton);
-        JButton ExecButton = new JButton("ExecXplor");
-        panel_button.add(ExecButton);
+        c.gpanel.setupGraphStyle(25,32,2,false);
+        JPanel p;
+        p = new CtrlInputPanel(c.gpanel,dbio).panel;
         frame.getContentPane().add(c,BorderLayout.CENTER);
-        frame.getContentPane().add(panel_button, BorderLayout.SOUTH);
-        frame.getContentPane().add(new InputPanel().panel,BorderLayout.EAST);
+        frame.getContentPane().add(p,BorderLayout.EAST);
+
         frame.pack();
         frame.setSize( 820,600);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        frame.addWindowListener(new WindowAdapter()
+        {
+            @Override
+            public void windowClosing(WindowEvent e)
+            {
+                System.out.println("Closing");
+                dbio.close();
+                e.getWindow().dispose();
+            }
+        });
+    }
+
+    public static void main(String[] args)
+    {
+        crossLinkingUI ui=new crossLinkingUI();
     }
 }
